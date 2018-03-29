66daf8c60e941d1d6a6f27ac90203ba2cf972d64
Svetlana <svetaa-05@yandex.ru>
2018-03-29 09:19:27 +0300

    lesson 6 homework. JDBC.

---
+++ src/main/java/lesson6/Configuration.java
@@ -0,0 +1,50 @@
+package lesson6;
+
+public class Configuration {
+
+    private String database;
+    private String host;
+    private String port;
+    private String username;
+    private String password;
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
